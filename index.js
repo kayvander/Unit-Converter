2954a1acaf4d69ef74f074d837ff226e49c309f8
Mikayla Vanderveen <199833538+kayvander@users.noreply.github.com>
2025-03-19 17:21:52 -0400

    edits

--- index.js
+++ index.js
@@ -12,12 +12,33 @@ let volumeEl = document.getElementById("liter-gallon");
 let massEl = document.getElementById("kilo-pounds");
 let inputEl = document.getElementById("num-input")
 let convertBtn = document.getElementById("convert-btn")
+let saveBtn = document.getElementById("save-btn")
 
 convertBtn.addEventListener("click", function() {
     let baseValue = inputEl.value;
-    
-    lengthEl.textContent = `${baseValue} meters = ${(baseValue * meterToFeet).toFixed(3)} feet | ${baseValue} feet = ${(baseValue / meterToFeet).toFixed(3)} meters`
-    volumeEl.textContent = `${baseValue} liters = ${(baseValue * literToGallon).toFixed(3)} gallons | ${baseValue} gallons = ${(baseValue / literToGallon).toFixed(3)} liters`
-    massEl.textContent = `${baseValue} kilograms = ${(baseValue * kiloToPound).toFixed(3)} pounds | ${baseValue} pounds = ${(baseValue / kiloToPound).toFixed(3)} kilograms`
+    let conversionResults = {
+        length: `${baseValue} meters = ${(baseValue * meterToFeet).toFixed(3)} feet | ${baseValue} feet = ${(baseValue / meterToFeet).toFixed(3)} meters`,
+        volume: `${baseValue} liters = ${(baseValue * literToGallon).toFixed(3)} gallons | ${baseValue} gallons = ${(baseValue / literToGallon).toFixed(3)} liters`,
+        mass: `${baseValue} kilograms = ${(baseValue * kiloToPound).toFixed(3)} pounds | ${baseValue} pounds = ${(baseValue / kiloToPound).toFixed(3)} kilograms`
+    };
+
+    lengthEl.textContent = conversionResults.length;
+    volumeEl.textContent = conversionResults.volume;
+    massEl.textContent = conversionResults.mass;
+})
+
+localStorage.getItem("conversionResults")
+
+saveBtn.addEventListener("click", function() {
+    let savedItem = document.getElementById("ul-el");
+    savedItem.innerHTML= `
+        <h4>Saved Length</h4>
+        <p>${lengthEl.textContent}</p>
+        <h4>Saved Volume</h4>
+        <p>${volumeEl.textContent}</p>
+        <h4>Saved Mass</h4>
+        <p>${massEl.textContent}</p>`;
+    ulEl.appendChild(savedItem);
+    if (savedItem.inn)
 })
 
